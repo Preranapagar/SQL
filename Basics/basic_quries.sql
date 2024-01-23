18b15cd7009c0fedd3db31cbbf8afc0480fc2b16
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-01-23 12:57:36 +0530

    Create basic_quries.sql

---
+++ Basics/basic_quries.sql
@@ -0,0 +1,53 @@
+-- SQL is not case sensitive so we can use upper as well as lower case to write queries
+
+-- to create a database
+
+create database temp;
+create database collage;
+create database if not exists collage;
+-- to drop database
+
+drop database medline;
+drop database db_jan;
+drop database temp;
+drop database if exists temp2;
+
+-- to check databases in server
+
+show databases;
+
+-- to use database
+
+use collage;
+
+-- to create table
+
+create table student(
+	ID int primary key,
+    name varchar(50),
+    Age int not null
+);
+
+-- to insert data into table
+insert into student values (1,'Aman',25);
+insert into student values (2,'Ram',45);
+
+-- to check the tables
+
+show tables;
+
+-- to select and view all columns and rows
+
+select * from student;
+
+-- to drop table
+
+drop table student;
+
+-- creating new table
+
+create table students(
+Rollno int primary key,
+Name varchar(50)
+);
+
